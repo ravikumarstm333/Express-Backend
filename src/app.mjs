7a9745c037f4eaa6e8c3585ca2f1d2889c2fb930
Ravi Kumar <ravikumarstm333@email.com>
2026-08-19 12:02:17 +0530

    first comment

---
+++ src/app.mjs
@@ -0,0 +1,9 @@
+import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config()
+const app=express();
+
+const PORT=process.env.PORT 
+app.listen(PORT,(err)=>{
+    console.log(`Sever runnign on ${PORT}`);
+})
