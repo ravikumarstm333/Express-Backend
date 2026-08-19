fc199d294a636509b94fbbaaddb688051f93c7cf
Ravi Kumar <ravikumarstm333@email.com>
2026-08-19 12:45:22 +0530

    learn how to make router

--- src/app.mjs
+++ src/app.mjs
@@ -1,9 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv';
+
 dotenv.config()
 const app=express();
-
-const PORT=process.env.PORT 
+app.use(express.static('src/public'));
+app.get('/',(req,res)=>{
+    res.send("Express Server Started... "); //-> this work like noaml method (write end )
+})
+app.get('/*splat',(req,res)=>{
+    res.status(404).sendFile('/404.html',{
+        root:'src/public'
+    });
+})
+const PORT=Number(process.env.PORT )
 app.listen(PORT,(err)=>{
     console.log(`Sever runnign on ${PORT}`);
 })
